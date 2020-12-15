1e9ec1b6c45ea5fb516562ca8c641fef082ff20b
Aleksandr70711 <75685882+Aleksandr70711@users.noreply.github.com>
2020-12-15 18:37:58 +0300

    test

---
+++ ПанфиловАлександр/Assets/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+
+{    public Text score;
+     public void Endgame()
+    {
+      Debug.Log("GAME OVER");
+      score.text = "GAME OVER";
+    }
+
+ 
+}
---
+++ ПанфиловАлександр/Assets/PlayerCollision.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // Start is called before the first frame update
+public PlayerMovement playerMovement;
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+     
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            Debug.Log("Ай,Больно!");
+            playerMovement.enabled = false;
+            FindObjectOfType<GameManager>().Endgame();
+        }
+    }
+}
